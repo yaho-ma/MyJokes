6679e5c90e5d89666a752cf0e42a268734d31f3f
archemax <dev76913@gmail.com>
2024-08-28 12:58:42 +0300

    Add project files.

---
+++ MyJokes/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyJokes.Models;
+
+namespace MyJokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyJokes.Models.Joke> Joke { get; set; } = default!;
+    }
+}
